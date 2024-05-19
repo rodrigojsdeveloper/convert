50b0a052521390d7122e70c0b6603dfc2fca26ab
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-05-19 16:49:53 -0300

    feat: add to convert options

--- src/components/custom/select.tsx
+++ src/components/custom/select.tsx
@@ -1,16 +1,16 @@
 'use client'
-// interface SelectProps {}
 
 import { useEffect, useRef, useState } from 'react'
-import { ChevronDown } from 'lucide-react'
+import { Check, ChevronDown } from 'lucide-react'
 import { cn } from '@/utils/cn'
+import { CURRENCY_RATES } from '@/utils/constants'
 
 export const Select = () => {
   const [closeSelect, setCloseSelect] = useState(false)
+  const [selectedCurrency, setSelectedCurrency] = useState<string | null>(null)
   const selectRef = useRef<HTMLDivElement>(null)
 
   const handleClickOutside = (event: MouseEvent) => {
-    setCloseSelect(true)
     if (
       selectRef.current &&
       !selectRef.current.contains(event.target as Node)
@@ -24,7 +24,12 @@ export const Select = () => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside)
     }
-  })
+  }, [])
+
+  const handleSelect = (currency: string) => {
+    setSelectedCurrency(currency)
+    setCloseSelect(false)
+  }
 
   return (
     <div
@@ -33,15 +38,42 @@ export const Select = () => {
         'relative flex h-[3.25rem] w-full cursor-pointer flex-row items-center justify-between rounded-lg border border-solid border-blue-400 bg-blue-200 px-5 py-4 text-white-100',
         closeSelect && 'border-blue-700',
       )}
+      onClick={() => setCloseSelect(!closeSelect)}
     >
-      <label className="cursor-pointer text-blue-900">Selecione a moeda</label>
+      <label
+        className={cn(
+          'cursor-pointer text-blue-900',
+          selectedCurrency ? 'text-white-100' : 'text-blue-900',
+        )}
+      >
+        {selectedCurrency || 'Selecione a moeda'}
+      </label>
       {closeSelect && (
-        <ul className="absolute right-0 top-16 z-10 h-[14.75rem] w-full max-w-[22rem] rounded-lg border border-solid border-blue-400 bg-blue-300"></ul>
+        <ul className="absolute right-0 top-16 z-10 w-full max-w-[22rem] rounded-lg border border-solid border-blue-400 bg-blue-300">
+          {CURRENCY_RATES.map((currency, index) => (
+            <li
+              key={`${currency.currency}-%${index}`}
+              className={cn(
+                'flex cursor-pointer flex-row items-center justify-between px-5 py-3',
+                selectedCurrency === currency.currency
+                  ? 'text-white-100'
+                  : 'text-blue-900',
+              )}
+              onClick={() => handleSelect(currency.currency)}
+            >
+              {currency.currency}
+              {selectedCurrency === currency.currency && (
+                <Check className="size-5 text-white-100" />
+              )}
+            </li>
+          ))}
+        </ul>
       )}
       <ChevronDown
         className={cn(
           'absolute right-4 size-5 text-blue-900 transition-transform duration-200',
           closeSelect && 'rotate-180 text-blue-700',
+          selectedCurrency ? 'text-white-100' : 'text-blue-900',
         )}
       />
     </div>
---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+export const CURRENCY_RATES = [
+  {
+    currency: 'Dólar americano',
+    code: 'USD',
+    rate: 5.2,
+  },
+  {
+    currency: 'Euro',
+    code: 'EUR',
+    rate: 5.5,
+  },
+  {
+    currency: 'Iene japonês',
+    code: 'JPY',
+    rate: 0.038,
+  },
+  {
+    currency: 'Peso argentino',
+    code: 'ARS',
+    rate: 0.028,
+  },
+  {
+    currency: 'Yuan chinês',
+    code: 'CNY',
+    rate: 0.8,
+  },
+]
